789c359f198a4ce37e34b5b2d4bd366f5b46607c
Lwrence <lawrencemalaga3@gmail.com>
2025-11-12 23:27:23 +0800

    first commit for feature/appointment

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace ClinicManagementSystem;
+namespace Clinic_Management_System;
 
 /// <summary>
 /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ClinicManagementSystem;
+namespace Clinic_Management_System;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
