0c29f28042b1a44ead5cc91db0e0e2b8f76e7a67
Augustine <qalbarredo@tip.edu.ph>
2026-05-16 23:44:05 +0800

    refactored and separated overlay forms in user management as separate files for clean and easy debugging

---
+++ Views/UserManagement/UpdateUserOverlayView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CruzNeryClinic.Views.UserManagement
+{
+    public partial class UpdateUserOverlayView : UserControl
+    {
+        public UpdateUserOverlayView()
+        {
+            InitializeComponent();
+        }
+    }
+}
