6eee61a40fd7e31048293e5fa13655faa24851df
Augustine <qalbarredo@tip.edu.ph>
2026-06-10 18:31:20 +0800

    Maintenance userfriendly for backup

--- Views/MaintenanceView.xaml.cs
+++ Views/MaintenanceView.xaml.cs
@@ -1,13 +1,46 @@
 using CruzNeryClinic.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CruzNeryClinic.Views
 {
     public partial class MaintenanceView : UserControl
     {
+        private bool isClearingBackupPasswordInput;
+
         public MaintenanceView()
         {
             InitializeComponent();
+        }
+
+        private void BackupPasswordInput_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (isClearingBackupPasswordInput)
+                return;
+
+            if (DataContext is MaintenanceViewModel viewModel)
+                viewModel.BackupPasswordPromptPassword = BackupPasswordInput.Password;
+        }
+
+        private void BackupPasswordPromptOverlay_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (BackupPasswordInput == null)
+                return;
+
+            if (e.NewValue is true)
+            {
+                isClearingBackupPasswordInput = true;
+                BackupPasswordInput.Password = string.Empty;
+                isClearingBackupPasswordInput = false;
+
+                BackupPasswordInput.Focus();
+            }
+            else
+            {
+                isClearingBackupPasswordInput = true;
+                BackupPasswordInput.Password = string.Empty;
+                isClearingBackupPasswordInput = false;
+            }
         }
     }
 }
